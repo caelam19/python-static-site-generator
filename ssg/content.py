41ea4b9f7af70edb0b9cf11b60ad1127c2e378e6
caelam19 <109554199+caelam19@users.noreply.github.com>
2022-07-22 16:17:45 -0400

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -3,11 +3,12 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*S"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
-
-    def load(self, cls, string):
-        _, fm, content = __regex.split(string, 2)
+    
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
 
         return cls(metadata, content)
