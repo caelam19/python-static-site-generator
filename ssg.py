40fd47ac7503bd74c9ec91910d978165bbd13984
caelam19 <109554199+caelam19@users.noreply.github.com>
2022-07-22 15:58:02 -0400

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -2,7 +2,7 @@ import typer
 import ssg.parsers
 from ssg.site import Site
 
-def main(source="content", dest="dists"):
+def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest, 'parsers': [ssg.parsers.ResourceParser(), ssg.parsers.ReStructuredTextParser()]}
     Site(**config).build()
 
